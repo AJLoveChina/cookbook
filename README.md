f83e2e86629e27fc3447ee949f8bb07928810905
Baker.He <Baker.He@zoom.us>
2019-04-10 16:57:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+## 莉雯小姐的私房食谱
+>菜谱大全，家常菜谱，美食问答，食谱分类，健康食疗，厨友排行
+
+
+## 文档地址
+[http://docs.nigeerhuo.com/cookbook](http://docs.nigeerhuo.com/cookbook)
+
+## 开发以及部署
+```bash
+yarn install
+yarn docs:dev
+```
+Then deloy
+```bash
+yarn docs:build
+```
+
+> 欢迎提交PR，我会每天check :joy:
+> 不必纠结写作的风格，任何风格都是可以接受的，而且我们会帮你修改。
---
+++ src/.vuepress/config.js
@@ -0,0 +1,39 @@
+let path = require("path");
+let assetsPath = path.resolve(__dirname, '../assets');
+
+module.exports = {
+    title: '莉雯小姐的私房食谱',
+    description: '食谱菜单，西红柿炒蛋，肉末茄子，牛肉炖土豆，小鸡炖蘑菇',
+    base: '/cookbook/',
+    dest: path.resolve(__dirname, "../../cookbook"),
+    head: [
+        ['link', {rel: 'shortcut icon', type: "image/x-icon", href: '/nigeerhuox16.ico'}],
+        ['script', {async : 'async', src: "/baidu-tongji.js"}],
+    ],
+    ga: "UA-70087663-3",
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@alias': assetsPath
+            }
+        }
+    },
+    themeConfig: {
+        displayAllHeaders: true, // 默认值：false
+        repo: "https://github.com/AJLoveChina/cookbook",
+        repoLabel: 'Github',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'src',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        // 默认为 "Edit this page"
+        editLinkText: '在 Github 上编辑此页',
+        editLinks: true,
+        lastUpdated: "最近一次更新时间",
+        sidebar: [
+            ["/", "首页"],
+            ['/xhscd/', '西红柿炒蛋'],
+        ]
+    }
+};
